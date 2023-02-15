453ba07afa4ff4babacaf9da7bf2446fa5a2c729
matthardy11 <matthardy2560@gmail.com>
2023-02-15 18:21:54 +0400

    complete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int M = 1;
+int N = 15;
+
+int SumNumberInRange(int startRange, int finishRange)
+{
+  if (startRange < finishRange)
+  {
+    startRange += SumNumberInRange(startRange+1, finishRange);
+    return startRange; 
+  }
+  return startRange;
+}
+
+System.Console.WriteLine($"Сумма чисел от {M} до {N} равна {SumNumberInRange(M, N)}");
